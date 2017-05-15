66e8b34790d699e04414aba6c5d284220d04c282
jane.yi <jane.yi@kilimall.com>
2017-05-15 10:05:31 +0800

    修改路由

---
+++ src/pages/index/children/lifestyle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lifestyle-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lifestyle'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/index/children/usercenter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="usercenter-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'usercenter'
+}
+</script>
+
+<style>
+
+</style>
